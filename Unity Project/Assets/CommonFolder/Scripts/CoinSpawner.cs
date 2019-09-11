30f1a8179acac3b6aeb3a37db6ef46f42d0dc2a1
blueboogie0552 <38578524+blueboogie0552@users.noreply.github.com>
2019-09-11 23:01:56 +0900

    Add CoinPusherBaseSystem
    Add Stage
    Add CoinSpawner

---
+++ Unity Project/Assets/CommonFolder/Scripts/CoinSpawner.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    public GameObject CoinPrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 clickPosition = Input.mousePosition;
+            clickPosition.z += 12f;
+            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(clickPosition);
+            Instantiate(this.CoinPrefab, worldPoint, Quaternion.identity);
+        }
+    }
+}
