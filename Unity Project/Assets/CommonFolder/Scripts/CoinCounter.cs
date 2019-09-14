9ee34ae381cb03cc64e9a7be4892e765e510a280
blueboogie0552 <38578524+blueboogie0552@users.noreply.github.com>
2019-09-14 18:32:00 +0900

    Add ScoreView and CoinSE
    Add ScoreView and CoinSE

---
+++ Unity Project/Assets/CommonFolder/Scripts/CoinCounter.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CoinCounter : MonoBehaviour
+{
+    public int scoreCount;
+    public Text scoreText;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.scoreCount = 50;
+        this.scoreText = this.GetComponent<Text>();
+        this.scoreText.text = this.scoreCount.ToString();
+    }
+
+    public void AddScore()
+    {
+        this.scoreCount++;
+        this.scoreText.text = this.scoreCount.ToString();
+    }
+
+    public void SubScore()
+    {
+        this.scoreCount--;
+        this.scoreText.text = this.scoreCount.ToString();
+    }
+    
+    public bool IsEmpty()
+    {
+        return this.scoreCount <= 0;
+    }
+}
--- Unity Project/Assets/CommonFolder/Scripts/CoinSpawner.cs
+++ Unity Project/Assets/CommonFolder/Scripts/CoinSpawner.cs
@@ -5,26 +5,19 @@ using UnityEngine;
 public class CoinSpawner : MonoBehaviour
 {
     public GameObject CoinPrefab;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
     void Update()
     {
         if (Input.GetMouseButtonDown(0))
         {
-            Vector3 clickPosition = Input.mousePosition;
-            clickPosition.z += 12f;
-            Vector3 worldPoint = Camera.main.ScreenToWorldPoint(clickPosition);
-            Instantiate(this.CoinPrefab, worldPoint, Quaternion.identity);
-        }
-
-        if (this.CoinPrefab.transform.position.y < -10)
-        {
-            Destroy(this.CoinPrefab);
+            GameObject coinCounter = GameObject.Find("CoinCounter");
+            if (!coinCounter.GetComponent<CoinCounter>().IsEmpty())
+            {
+                coinCounter.GetComponent<CoinCounter>().SubScore();
+                Vector3 clickPosition = Input.mousePosition;
+                clickPosition.z += 12f;
+                Vector3 worldPoint = Camera.main.ScreenToWorldPoint(clickPosition);
+                Instantiate(this.CoinPrefab, worldPoint, Quaternion.identity);
+            }
         }
     }
 }
